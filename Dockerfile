d970bccfbed9074d6d032dcf6ac09c3dd7aa6ba6
4OH4 <4oh4_git@gmail.cm>
2021-02-27 18:21:39 +0000

    Add Docker healthcheck

--- Dockerfile
+++ Dockerfile
@@ -7,4 +7,8 @@ EXPOSE 8080
 
 COPY ./service /service
 
+# Use the ping endpoint as a healthcheck,
+# so Docker knows if the API is still running ok or needs to be restarted
+HEALTHCHECK --interval=21s --timeout=3s --start-period=10s CMD curl --fail http://localhost:8080/ping || exit 1
+
 CMD ["uvicorn", "service.main:app", "--host", "0.0.0.0", "--port", "8080"]
--- service/main.py
+++ service/main.py
@@ -12,15 +12,12 @@ app = FastAPI(
 app.include_router(api_router, prefix=API_V1_STR)
 
 
-@app.get("/ping")
+@app.get("/ping", summary="Check that the service is operational")
 def pong():
     """
-    Sanity check.
+    Sanity check - this will let the user know that the service is operational.
 
-    This will let the user know that the service is operational.
-
-    And this path operation will:
-    * show a lifesign
+    It is also used as part of the HEALTHCHECK. Docker uses curl to check that the API service is still running, by exercising this endpoint.
 
     """
     return {"ping": "pong!"}
